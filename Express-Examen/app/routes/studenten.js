c3df84bde7955b92da2512aa4179134ee002f268
Kristof De Winter <s106749@ap.be>
2020-01-20 11:48:52 +0100

    Changed get students
    Added sort functionality

--- Express-Examen/app/routes/studenten.js
+++ Express-Examen/app/routes/studenten.js
@@ -4,36 +4,40 @@ const MongoClient = require('mongodb').MongoClient
 var db;
 
 MongoClient.connect('mongodb://localhost:27017', (err, database) => {
-  if (err) return console.log(err)
-  db = database.db('exam')
+    if (err) return console.log(err)
+    db = database.db('exam')
 })
 
 /* GET ALL STUDENTS */
 router.get('/', (req, res) => {
     db.collection('students').find().toArray((err, result) => {
-      if (err) return
-      res.render('list.ejs', { students: result })
+        result = result.sort(function (a, b) {
+            var x = a.name < b.name ? -1 : 1;
+            return x;
+        })
+        if (err) return
+        res.render('list.ejs', { students: result })
     })
-  })
+})
 
 /* SHOW ADD FORM */
 router.get('/add', (req, res) => {
     res.render('add.ejs', {})
- })
+})
 
 /* ADD STUDENT */
 router.post('/add', (req, res) => {
-  var student = { naam: req.body.naam, geboortedatum: req.body.geboortedatum, studierichting: req.body.studierichting };
-  db.collection('students').findOne(student, (err, result) => {
-    if (err) return
-    if (result) {
-      res.render('studentexists.ejs', {})
-    } else {
-      db.collection('students').insertOne({ naam: req.body.naam, geboortedatum: req.body.geboortedatum, studierichting: req.body.studierichting }, (err, result) => {
-        res.redirect('/')
-      })
-    }
-  })
+    var student = { naam: req.body.naam, geboortedatum: req.body.geboortedatum, studierichting: req.body.studierichting };
+    db.collection('students').findOne(student, (err, result) => {
+        if (err) return
+        if (result) {
+            res.render('studentexists.ejs', {})
+        } else {
+            db.collection('students').insertOne({ naam: req.body.naam, geboortedatum: req.body.geboortedatum, studierichting: req.body.studierichting }, (err, result) => {
+                res.redirect('/')
+            })
+        }
+    })
 })
 
 module.exports = router;
